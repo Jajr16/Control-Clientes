cc28ff067b693a947b4404ad7f834e57106e3fba
W1NTER <83407276+ShabbyRug775@users.noreply.github.com>
2025-07-14 17:30:37 -0600

    Forms de inmueble
    Forms de los datos necesarios de cada inmueble.

--- frontend/src/pages/AgregarCliente.jsx
+++ frontend/src/pages/AgregarCliente.jsx
@@ -5,6 +5,10 @@ import DatoRegistralForm from "../components/forms/DatoRegistralForms.jsx";
 import DireccionForm from "../components/forms/DireccionForms.jsx";
 import EmpresaForm from "../components/forms/EmpresaForms.jsx";
 import PropietarioForm from "../components/forms/PropietarioForms.jsx";
+import InmuebleForm from "../components/forms/InmuebleForms.jsx";
+import InmuebleHipotecaForm from "../components/forms/InmuebleHipotecaForms.jsx";
+import InmuebleProveedorForm from "../components/forms/InmuebleProveedorForms.jsx";
+import InmuebleSeguroForm from "../components/forms/InmuebleSeguroForms.jsx";
 
 import { manejarLogicaCliente } from "../hooks/operacionesClienteForm.js";
 
@@ -20,8 +24,12 @@ const AddClientesPage = () => {
 
     // Estado para controlar la visibilidad del formulario principal
     const [mostrarFormulariosCliente, setMostrarFormulariosCliente] = useState(false);
+    const [mostrarFormulariosInmueble, setMostrarFormulariosInmueble] = useState(false);
     const toggleFormularioCliente = () => {
         setMostrarFormulariosCliente(!mostrarFormulariosCliente);
+    };
+    const toggleFormularioInmueble = () => {
+        setMostrarFormulariosInmueble(!mostrarFormulariosCliente);
     };
 
     return (
@@ -57,8 +65,49 @@ const AddClientesPage = () => {
                                 validationErrors={ValidarErrores}
                             />
                         </div>
+                        <div className="border border-black rounded-md p-2 col-span-2">
+                        {/* Formulario para Datos Registrales */}
+                        <DatoRegistralForm
+                            datoRegistral={datoRegistralEmpresa}
+                            setDatoRegistral={setDatoRegistralEmpresa}
+                            validationErrors={ValidarErrores}
+                        />
                     </div>
-                    <div></div>
+                    </div>
+                    
+                </div>
+                }
+            </div>
+            <div className="border border-black rounded-b-lg">
+                <div className="border-b border-black flex w-full justify-between">
+                    <strong className="text-xl ml-5">Inmuebles</strong>
+                    <RiArrowDownSLine className="h-6 w-6" onClick={toggleFormularioInmueble}/>
+                </div>
+                {mostrarFormulariosInmueble && 
+                <div className="">
+                    <div className="grid grid-cols-[48%_48%] mb-4 mt-4 w-full justify-evenly">
+                        <div className="border border-black rounded-md p-2">
+                            <strong className="text-lg">Inmueble</strong>
+                            <InmuebleForm
+                                validationErrors={ValidarErrores}
+                            />
+                            <InmuebleProveedorForm
+                                validationErrors={ValidarErrores}
+                            />
+                        </div>
+                        <div className="border border-black rounded-md p-2">
+                            <strong className="text-lg">Hipoteca</strong>
+                            <InmuebleHipotecaForm
+                                validationErrors={ValidarErrores}
+                            />
+                        </div>
+                        <div className="border border-black rounded-md p-2 col-span-2">
+                            <strong className="text-lg">Seguro</strong>
+                            <InmuebleSeguroForm
+                                validationErrors={ValidarErrores}
+                            />
+                        </div>
+                    </div>
                 </div>
                 }
             </div>
